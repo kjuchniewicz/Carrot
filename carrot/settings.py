a65112a392d0c059f16cc9bb49bb098cdc8b46f4
K.Juchniewicz <k.juchniewicz@zarna.pl>
2020-05-18 10:00:18 +0200

    INIT

--- carrot/settings.py
+++ carrot/settings.py
@@ -54,7 +54,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'template'),
+            os.path.join(BASE_DIR, 'blog/template'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
